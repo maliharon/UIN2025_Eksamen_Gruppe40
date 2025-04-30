b52d2e3b36048ea9559bfb2300f46bb654d4fac5
maliharon <142988041+maliharon@users.noreply.github.com>
2025-04-30 14:48:11 +0200

    start på app.jsx og Layout
    Meg og Malene sitter sammen på skolen og jobber. Vi har fulgt eksempler fra pensum på github

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Layout from "./component/Layout";
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  const [festivaler, setFestivaler] = useState();
 
+  // En funksjon som henter spilldata fra en API
+  const getFestivaler = async () => {
+    fetch("https://app.ticketmaster.com/discovery/v2/events?apikey=9ODnmhE238NznalEefKKa0oTfHnvc4Qn&keyword=Findings&locale=*") // Henter data fra API-et
+      .then((response) => response.json()) // Konverterer svaret til JSON-format
+      .then((data) => setFestivaler(data.data)) // Setter spillene i state-variabelen
+      .catch((error) => console.error("Skjedde noe dritt ved fetch", error)); // Hvis noe går galt, vis en feilmelding
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   <Layout>
+      
+      <Routes>
+      
+      </Routes>
+    </Layout>
     </>
   )
 }
---
+++ src/component/Layout.jsx
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom";
+
+export default Layout ({children}); {
+    return (
+        <>
+            <header>
+                <nav>
+                    <link to="/">Hjem</link>
+                    <link to="/">Hjem</link>
+                    <link to="/">Hjem</link>
+
+                </nav>
+            </header>
+        </>
+    )
+}
