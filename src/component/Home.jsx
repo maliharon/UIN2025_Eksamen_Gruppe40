2177fa636c78215fa09bed9f984708337d9c69ef
ThatsGreatDude <malenest@hiof.no>
2025-05-01 09:48:48 +0200

    huskeliste ting
    Bare påminnelse for oss selv så vi slipper å bytte faner hele tiden for å se på kravene

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,1 +1,4 @@
-//Forside
+{/*  Forside. Skal vise fire spesifikke festivaler hentet fra API-et:
+Findings, Neon, Skeikampenfestivalen, og Tons of Rock
+Disse skal vises som artikkelkort med navn og lenke til EventPage. 
+Dette er bare huskelista fra eksamen - Malene */}
